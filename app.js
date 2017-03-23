ae7aaafbadfa802e1801b52b291059c7b51590c4
Nick Mistretta <nickmistretta9@gmail.com>
2017-03-23 15:14:54 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,7 @@
+var router = require('./router.js');
+var http=require('http');
+http.createServer(function (request, response) {
+ router.home(request, response);
+ router.user(request, response);
+}).listen(3000);
+console.log('Server running at http://<workspace-url>/');
